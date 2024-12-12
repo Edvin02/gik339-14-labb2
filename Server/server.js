6da5d347b7465a1f3fff065066ce9061dba32790
Elange Franklin <114111024+Elangefranklin@users.noreply.github.com>
2024-12-12 18:28:44 +0100

    installerade npm express

--- Server/server.js
+++ Server/server.js
@@ -1,31 +1,34 @@
-const express = require('express')
+const express = require("express");
 const server = express();
-const sqlite3 = require('sqlite3');
+const sqlite3 = require("sqlite3").verbose();
 
 server
-     .use(express.json())
-     .use(express.urlencoded({ extended: false}))
-     .use((req, res, next) => {
-          res.header('Access-Control-Allow-Origin', '*');
-          res.header('Access-Control-Allow-Headers', '*');
-          res.header('Access-Control-Allow-Methods', '*');
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "*");
 
-          next();
-     });
+    next();
+  });
 
-server.get('/users', (req, res) => {
-     const db = new sqlite3.Database("./gik339-labb2.db");
+server.get("/users", (req, res) => {
+  const db = new sqlite3.Database("./gik339-labb2.db");
 
-     const sql = 'SELECT * FROM users';
+  const sql = "SELECT * FROM users";
 
-     db.all(sql, (err, rows) => {
-          if (err) {
-               return res.status(500).send({ error: 'Database error', details: err.message });
-          }
-          res.send(rows);
-     });
+  db.all(sql, (err, rows) => {
+    if (err) {
+      return res
+        .status(500)
+        .send({ error: "Database error", details: err.message });
+    }
+    res.send(rows);
+  });
+  db.close();
 });
 
 server.listen(3000, () =>
-     console.log('Running server on http://localhost:3000')
+  console.log("Running server on http://localhost:3000")
 );
