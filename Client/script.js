2709411f61e3b4f8220cdf5bd7056d65a869497d
Elange Franklin <114111024+Elangefranklin@users.noreply.github.com>
2024-12-17 09:09:04 +0100

    testade await

--- Client/script.js
+++ Client/script.js
@@ -1,5 +1,34 @@
-const localUrl = 'http://localhost:3000/users';
+// const localUrl = "http://localhost:3000/users";
 
-fetch(localUrl)
-     .then((response) => response.json())
-     .then((data) => console.log(data))
+// fetch(localUrl)
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+// Funktion för att hämta användardata från servern och visa det i gränssnittet
+
+async function fetchUsers() {
+  try {
+    const response = await fetch("http://localhost:3000/users");
+
+    const users = await response.json();
+
+    const userList = document.getElementById("user-list");
+
+    const ul = document.createElement("ul");
+
+    users.forEach((user) => {
+      const li = document.createElement("li");
+
+      li.style.color = user.color;
+      li.textContent = `${user.firstName} ${user.lastName} (${user.username})`;
+
+      ul.appendChild(li);
+    });
+
+    userList.appendChild(ul);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", fetchUsers);
